7541bad9e8e0fb273b974fc5588f29d21e25e803
Mac <mac@MacBook-Pro.fios-router.home>
2019-02-01 07:28:58 -0500

    update

--- web/src/components/redux/store/store.js
+++ web/src/components/redux/store/store.js
@@ -17,6 +17,9 @@ if (process.env.NODE_ENV === 'development') {
   // returns error when state is mutated
   middlewares.push(freeze);
   middlewares.push(logger);
+  console.log('development environment')
+} else {
+  console.log('production environment')
 }
 
 export function configureStore() {
